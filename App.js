101003b93f82f9920a907f241bbb072ca5bd5a0e
sudhir.pw <skt2ktiwari@gmail.com>
2023-11-28 13:47:16 +0530

    custom components with props

--- App.js
+++ App.js
@@ -3,25 +3,28 @@
 import ActivityIndicatorUi from "./components/ActivityIndicator";
 import AlartUi from "./components/Alart";
 import ButtonUi from "./components/Button";
+import Greet from "./components/Greet";
 import ImageUi from "./components/Image";
 import ModalUi from "./components/Modal";
 import PressableUi from "./components/Pressable";
 import StatusBarUi from "./components/StatusBar";
 import TextUi from "./components/Text";
 import ViewUi from "./components/View";
+import {View} from 'react-native'
 
 
 export default function App() {
-  return (
-    
-    // <ViewUi/>
+  return <View style={{flex:1, backgroundColor:"plum", padding:60}}>
+    {/* // <ViewUi/>
     // <ImageUi/>
     // <ButtonUi/>
     // <PressableUi/>
     // <ModalUi/>
     // <StatusBarUi/>
     // <ActivityIndicatorUi/>
-    <AlartUi/>
-    
-  );
+    // <AlartUi/> */}
+    <Greet name="sudhir"/>
+    <Greet name="Awneesh"/>
+    </View>
+  ;
 }
---
+++ components/Greet.js
@@ -0,0 +1,10 @@
+import { View ,Text } from "react-native"
+
+
+export default function Greet ({name}){
+    return <View >
+        <Text>Hello,{name}</Text>
+        
+
+    </View>
+}
