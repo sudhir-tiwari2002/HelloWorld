b0983ede4a46042de123ce3df9077b32e49bd64b
sudhir.pw <skt2ktiwari@gmail.com>
2023-11-28 11:27:42 +0530

    text ui component

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import { View } from "react-native";
+import { View, Text } from "react-native";
 
-
-export default function App(){
-  return <View style={{flex:1 , backgroundColor:'plum'}}> 
-              <View style={{backgroundColor:'lightblue' , width:200 , height:200}}></View>
-              <View style={{backgroundColor:'lightgreen' , width:200 , height:200}}></View>
-              <View style={{backgroundColor:'lightblue' , width:200 , height:200}}></View>
- </View>
- 
+export default function App() {
+  return (
+    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
+      <Text style={{alignContent:"center",color:'white'}}>this is first text </Text>
+      <Text>
+        It is a long established fact that a reader will be distracted by the
+        readable content of a page when looking at its layout. The point of
+        using Lorem Ipsum is that it has a more-or-less normal distribution of
+        letters, as opposed to using 'Content here, content here', making it
+        look like readable English. Many desktop publishing packages and web
+        page editors now use Lorem Ipsum as their default model text, and a
+        search for 'lorem ipsum' will uncover many web sites still in their
+        infancy. Various versions have evolved over the years, sometimes by
+        accident, sometimes on purpose (injected humour and the like).
+      </Text>
+    </View>
+  );
 }
